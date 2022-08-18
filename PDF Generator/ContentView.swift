770bad67ecaedd6d6016f0b2698db97386674efb
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-08-18 11:27:06 +0800

    Initial Commit

---
+++ PDF Generator/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PDF Generator
+//
+//  Created by Jia Chen Yee on 18/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PDF Generator/PDF_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_GeneratorApp.swift
+//  PDF Generator
+//
+//  Created by Jia Chen Yee on 18/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PDF_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
